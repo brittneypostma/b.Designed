50581b500fd4105733e7ce6dacec67bbad4cb1f1
Brittney Postma <brittney.postma@hotmail.com>
2019-07-26 16:15:53 -0400

    added changing text to homepage

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,8 +3,10 @@ import Layout from "../components/layout"
 
 const AboutPage = () => (
   <Layout>
-    <h1>About</h1>
-    <h2>I am a mom to 3 wonderful children and I love coding.</h2>
+    <h1>About Me</h1>
+    <h2>
+      I am a mom of <b>three</b> wonderful children{" "}
+    </h2>
   </Layout>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,42 @@ import Layout from "../components/layout"
 
 import "../styles/index.scss"
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hello,</h1>
-    <h2>I'm Brittney, a mom who loves to code.</h2>
-  </Layout>
-)
+const changingTextArray = [
+  "Mom",
+  "Developer",
+  "Designer",
+  "Marketer",
+  "Freelancer",
+]
+class IndexPage extends React.Component {
+  constructor() {
+    super()
+    this.state = { textIdx: 0 }
+  }
+
+  componentDidMount() {
+    this.timeout = setInterval(() => {
+      let curIdx = this.state.textIdx
+      this.setState({ textIdx: curIdx + 1 })
+    }, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timeout)
+  }
+
+  render() {
+    let changingText =
+      changingTextArray[this.state.textIdx % changingTextArray.length]
+    return (
+      <Layout>
+        <h1>Hello,</h1>
+        <h2>
+          I am a <span className="changing-text">{changingText}</span>
+        </h2>
+      </Layout>
+    )
+  }
+}
 
 export default IndexPage
