d8800f97943ac712781c17635839529df6d7275b
Brittney Postma <brittney.postma@hotmail.com>
2019-07-31 22:14:26 -0400

    working on modal

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,8 +1,31 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-const Modal = ({ isShowing, hide }) =>
-  isShowing
+const imagesArray = [
+  {
+    id: 0,
+    title: "Crwn Clothing",
+    src: require(`../images/sites/crwn.jpg`),
+    description:
+      "A mock store app built using React, Redux with Google Authentication and hosted on Heroku.",
+    href: "https://crwn-clothing-react.herokuapp.com",
+  },
+  {
+    title: "Crwn g",
+    src: require(`../images/sites/crwn.jpg`),
+    description:
+      "A mock store app built using React, Redux with Google Authentication and hosted on Heroku.",
+    href: "crwn-clothing-react.herokuapp.com",
+  },
+]
+
+const title = imagesArray.title
+const src = imagesArray.src
+const description = imagesArray.description
+const href = imagesArray.href
+
+const Modal = ({ isShowing, hide, title, src, description, href }) => {
+  return isShowing
     ? ReactDOM.createPortal(
         <React.Fragment>
           <div className="modal-overlay" />
@@ -25,12 +48,29 @@ const Modal = ({ isShowing, hide }) =>
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
-              <p>Hello, I'm a modal.</p>
+              {imagesArray.map(({ title, src, description, href }) => {
+                return (
+                  <div>
+                    <h1 className="modal-title">{title}</h1>
+                    <img className="modal-img" src={src} alt="img" />
+                    <p className="modal-text">{description}</p>
+                    <a
+                      href={href}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="modal-link"
+                    >
+                      <button className="modal-btn">View Site</button>
+                    </a>
+                  </div>
+                )
+              })}
             </div>
           </div>
         </React.Fragment>,
         document.body
       )
     : null
+}
 
 export default Modal
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -10,72 +10,81 @@ import Monsters from "../images/sites/monsters.jpg"
 import Piano from "../images/sites/piano.jpg"
 import Quote from "../images/sites/quote.jpg"
 import SignUp from "../images/sites/sign-up-form.jpg"
-import Visitor from "../images/sites/visitor.jpg"
 import Waukazoo from "../images/sites/wo.jpg"
 
 const Portfolio = () => {
   const { isShowing, toggle } = useModal()
+
   return (
     <Layout>
+      <Modal isShowing={isShowing} hide={toggle} />
       <div className="portfolio-page">
         <h1>Some of My Work</h1>
       </div>
       <div className="portfolio">
-        <div className="left-top-p diamond-button">
-          <img
-            src={Crwn}
-            alt="crwn-clothing"
-            className="crwn"
-            onClick={toggle}
-          />
-        </div>
-        <div className="right-top-p diamond-button">
-          <img
-            src={SignUp}
-            alt="sign up form"
-            className="sign-up"
-            onClick={toggle}
-          />
+        <div className="top-p">
+          <div className="left-top-p diamond-button">
+            <img
+              src={Crwn}
+              alt="crwn-clothing"
+              className="crwn portfolio-pics"
+              onClick={toggle}
+            />
+          </div>
+          <div className="right-top-p diamond-button">
+            <img
+              src={SignUp}
+              alt="sign up form"
+              className="sign-up portfolio-pics"
+              onClick={toggle}
+            />
+          </div>
         </div>
         <div className="middle-p-container">
           <div className="left-middle-p diamond-button">
             <img
               src={Waukazoo}
               alt="Waukazoo Visitor Sign In"
-              className="wo"
+              className="wo portfolio-pics"
               onClick={toggle}
             />
           </div>
           <div className="middle-p diamond-button">
             <img
-              src={Quote}
-              alt="random quote generator"
-              className="quote"
+              src={Piano}
+              alt="piano"
+              className="piano portfolio-pics"
               onClick={toggle}
             />
           </div>
           <div className="right-middle-p diamond-button">
             <img
-              src={Visitor}
-              alt="Waukazoo Visitor Sign In"
-              className="visitor"
+              src={DrumKit}
+              alt="Drum Kit"
+              className="drum portfolio-pics"
               onClick={toggle}
             />
           </div>
         </div>
-        <div className="left-bottom-p diamond-button">
-          <img
-            src={Monsters}
-            alt="monsters roladex"
-            className="monsters"
-            onClick={toggle}
-          />
-        </div>
-        <div className="right-bottom-p diamond-button">
-          <img src={Piano} alt="piano" className="piano" onClick={toggle} />
+        <div className="bottom-p">
+          <div className="left-bottom-p diamond-button">
+            <img
+              src={Monsters}
+              alt="monsters roladex"
+              className="monsters portfolio-pics"
+              onClick={toggle}
+            />
+          </div>
+          <div className="right-bottom-p diamond-button">
+            <img
+              src={Quote}
+              alt="random quote generator"
+              className="quote portfolio-pics"
+              onClick={toggle}
+            />
+          </div>
         </div>
       </div>
-      <Modal isShowing={isShowing} hide={toggle} />
     </Layout>
   )
 }
